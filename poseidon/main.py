8870152da2b367b772be9a79c1bdce49a360279e
Pablo Ruiz Ponce <pabloruizp00@gmail.com>
2022-11-20 01:39:17 +0100

    Basic Text2Image generation using diffusers

--- poseidon/main.py
+++ poseidon/main.py
@@ -1,2 +1,20 @@
+import torch
+from diffusers import StableDiffusionPipeline
+import matplotlib.pyplot as plt
+from PIL import Image
+
+
 if __name__ == '__main__':
-    print('POSEIDON')
+    # Load Model
+    pipe = StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5")
+
+    # Prompt
+    prompt = "a photo of an astronaut riding a horse on mars"
+
+    # Image generation
+    image = pipe(prompt).images[0]  
+    image.save('result.jpg')
+
+    # Plot
+    plt.imshow(image)
+    plt.show()
