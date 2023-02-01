1a0caf9dc8b5edcda6bb18ad5ee5f19967573c2b
Pablo Ruiz Ponce <pabloruizp00@gmail.com>
2023-02-01 13:30:06 +0100

    First functional version of the mixing algoritm

--- poseidon/generation/coco_instance_generator.py
+++ poseidon/generation/coco_instance_generator.py
@@ -52,7 +52,7 @@ class COCOInstanceGenerator(InstanceGenerator):
                         ignore=shutil.ignore_patterns('.*'))
 
         print("Copy Created")
-
+        
         # Read annotations as a dictionary
         with open(self.a_train_annotations_path) as f:
             self.train_annotations = json.load(f)
@@ -79,6 +79,16 @@ class COCOInstanceGenerator(InstanceGenerator):
                 print("0")
         # Save the count distance between the minirity classes and the majority class
         self.class_count_difference = df_counts.max() - df_counts
+        return
+    
+    def update_class_count_difference(self): 
+        # Load annotations into a dataframe
+        df = pd.DataFrame(self.train_annotations['annotations'])
+        # Obtain pd.Series with the count of the different rows
+        df_counts = df['category_id'].value_counts()
+        # Save the count distance between the minirity classes and the majority class
+        self.class_count_difference = df_counts.max() - df_counts
+        return
 
     def box_collider(self, x1, x2):
         return(x1.x < x2.x + x2.w and
@@ -105,71 +115,105 @@ class COCOInstanceGenerator(InstanceGenerator):
         bboxs = pd.DataFrame(bboxs, columns=['x', 'y', 'w', 'h'])
         return bboxs['y'].max()
 
-    def add_instance_image(self, img_row, instances_path):
+
+    def add_instance_image(self, img_row, instances_path, image, max_y, angle_camera_bin):
         population = self.class_count_difference.keys().to_numpy()
         weights = self.class_count_difference.to_numpy()
         choice = random.choices(population, weights=weights)[0]
+        self.class_count_difference[choice] = self.class_count_difference[choice] - 1
 
+        instances_path = os.path.join(instances_path, str(choice), str(angle_camera_bin))
+        instance_path = os.path.join(instances_path, random.choice(os.listdir(instances_path)))
+        instance = Image.open(instance_path)
+        
+        annotations = pd.DataFrame(self.train_annotations['annotations'])
+        
+        instance_id = annotations['id'].max() + 1
+        instance_image = img_row['id']
+        instance_bbox = [
+                            random.randint(0, img_row['width']),
+                            random.randint(max_y, img_row['height']),
+                            instance.width,
+                            instance.height
+                        ]
+        instance_area = instance.width * instance.height
+        instance_category = choice
+
+        instance_dict = {
+            "id": instance_id, 
+            "image_id": instance_image, 
+            "bbox": instance_bbox, 
+            "area": instance_area, 
+            "category_id": instance_category
+        }
+
+        image.paste(instance, box=(random.randint(0, img_row['width']), 
+                                    random.randint(max_y, img_row['height'])))
+
+        return instance_dict
+    
+    def get_new_instance_id(self):
+        annotations = pd.DataFrame(self.train_annotations['annotations'])
+        return (annotations['id'].max() + 1)
+
+    def add_instances_image(self, img_row, instances_path, iteration):
         if img_row["meta"] is not None and "gimbal_heading(degrees)" in img_row["meta"]:
+
             angle_camera = img_row["meta"]["gimbal_heading(degrees)"]
             # TODO: El 8 esta metido a palo y eso no esta bien
             angle_camera_bin = int(angle_camera) % 8
-            instances_path = os.path.join(instances_path, str(choice), str(angle_camera_bin))
-            instance_path = os.path.join(instances_path, random.choice(os.listdir(instances_path)))
-            
-            #img_path = os.path.join(self.images_path, 'train', img_row['file_name'])
             img_path = os.path.join(self.images_a_train_path, img_row['file_name'])
-            
-            instance = Image.open(instance_path)
             image = Image.open(img_path)
-            
             max_y = self.get_max_y_image(img_row)
+
+
+            # Update new image metadata
+            new_image_metadata = img_row.copy()
+            images_metadata = pd.DataFrame(self.train_annotations['images'])
+            image_id = images_metadata['id'].max() + 1
+            new_image_metadata['id'] = image_id
+            new_image_metadata['file_name'] =  str(iteration) + "_" + img_row['file_name']
+            self.train_annotations['images'].append(new_image_metadata.to_dict())
+
+            # Add the new instances
             annotations = pd.DataFrame(self.train_annotations['annotations'])
+            annotations_img =  annotations[annotations['image_id'] == img_row['id']].copy()
+            annotations_img['image_id'] = image_id
+            annotations_img['id'] = annotations_img['id'].apply(lambda x: self.get_new_instance_id())
+            self.train_annotations['annotations'].extend(annotations_img.to_dict('records'))
+            self.update_class_count_difference()
+
+            # Gets the number of instances that will be generated for each image
+            n_instances = random.randint(1, 10)
+            instances_metadata = []
+            for i in range(n_instances):
+                instance_metadata = self.add_instance_image(new_image_metadata, instances_path, image, max_y, angle_camera_bin)
+                instances_metadata.append(instance_metadata)
             
-            instance_id = annotations['id'].max() + 1
-            instance_image = img_row['id']
-            instance_bbox = [
-                                random.randint(0, img_row['width']),
-                                random.randint(max_y, img_row['height']),
-                                instance.width,
-                                instance.height
-                            ]
-            instance_area = instance.width * instance.height
-            instance_category = choice
-
-            instance_dict = {
-                "id": instance_id, 
-                "image_id": instance_image, 
-                "bbox": instance_bbox, 
-                "area": instance_area, 
-                "category_id": instance_category
-            }
-
-            image.paste(instance, box=(random.randint(0, img_row['width']), 
-                                       random.randint(max_y, img_row['height'])))
-
             # Update everything
-            image.save(os.path.join(self.images_a_train_path,img_row['file_name']))
-            self.train_annotations['annotations'].append(instance_dict)
-            self.class_count_difference[choice] = self.class_count_difference[choice] - 1
+            image.save(os.path.join(self.images_a_train_path,new_image_metadata['file_name']))
+            self.train_annotations['annotations'].extend(instances_metadata)
 
         return
 
     def balance(self, instances_path):
         self.dataset_stats()
         # Iterate until the dataset has been balanced
+        iteration = 1
+        images = pd.DataFrame(self.train_annotations['images'])
         while self.class_count_difference.sum() != 0:
             print("Class Balance")
             print(self.class_count_difference)
-            
-            images = pd.DataFrame(self.train_annotations['images'])
+
             # Fancier
-            print("Extracting Instances:")
+            print(f"Iteration {iteration}")
             tqdm.pandas()
             # New instances generation
-            images.progress_apply(lambda x: self.add_instance_image(x, instances_path), axis=1)
-            
+            images.progress_apply(lambda x: self.add_instances_image(x, instances_path, iteration), axis=1)
+
             with open(self.a_train_annotations_path, 'w') as f:
                 json.dump(self.train_annotations, f,  cls=NpEncoder)
+            
+            iteration = iteration + 1
         
         return
